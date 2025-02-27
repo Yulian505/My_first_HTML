1e6dca92b5772ed22c7cb0d6570ad03259eca1b3
Yulian505 <yuliancanul5@gmail.com>
2025-02-26 22:31:33 -0600

    Animation again

--- animation.js
+++ animation.js
@@ -11,3 +11,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
     images.forEach(img => observer.observe(img));
 });
+
+const button = document.querySelector("input");
+
+button.addEventListener("click", () => {
+    button.style.transform = "scale(0.9)";
+
+    setTimeout(() => {
+        button.style.transform = "scale(1.1)"; // Rebote hacia afuera
+    }, 100);
+
+    setTimeout(() => {
+        button.style.transform = "scale(1)"; // Vuelve a su tamaño normal
+    }, 200);
+
+    // Destello al hacer clic
+    button.style.animation = "flash 0.5s";
+    setTimeout(() => {
+        button.style.animation = ""; // Quita la animación después de un ciclo
+    }, 500);
+});
